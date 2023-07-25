4d321ca13c3bb1eaa06ee82c214269080cc2e824
ByronYe <byron.ye.mail@gmail.com>
2023-07-26 02:24:16 +0800

    - updated axios, add get methods

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,17 +1,28 @@
 const axios = require('axios').default
 
-function get ({ url } = {}) {
-  axios.get(url || 'http://www.baidu.com')
-    .then(function (response) {
-      console.log(response)
-      console.log(1)
+const HEADER = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+  Referer: 'http://www.baidu.com/'
+}
+
+function get ({ url, params, headers } = {}) {
+  return new Promise((resolve, reject) => {
+    axios.get(url || 'http://www.baidu.com', {
+      headers: { ...HEADER, ...headers },
+      params
     })
-    .catch(function (error) {
-      console.log(error)
-      console.log(1)
-    })
-    .finally(function () {
-    })
+      .then(result => {
+        const { data, status } = result
+        if (status === 200) {
+          resolve(data)
+        } else {
+          reject(new Error(`status: ${status}`))
+        }
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
 }
 
 const http = {
